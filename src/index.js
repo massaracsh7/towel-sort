26ffa2ad1d1d1cbae78bd90590caf41ae748fedd
Your Name <you@example.com>
2022-12-27 12:20:59 +0300

    feat: add towelSort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix = []) {
+        const arr = [];
+        for (i = 0; i < matrix.length; i++) {
+            if ((i % 2 === 0) || (i === 0)) {
+                let even = matrix[i];
+                arr.push(...even);
+            } else {
+                let odd = [...matrix[i]].reverse();
+                arr.push(...odd);
+            }
+        }
+
+        if (matrix.length !== 0) {
+            return arr;
+        } else {
+            return [];
+        }
 }
